a23a8672543e94fb05dc9c1d8043ecc05ddb53b6
Konner1 <konnerkinghorn@gmail.com>
2025-05-01 18:57:59 -0600

    Followed the video to get the piece positions set up

---
+++ shared/src/main/java/chess/PieceMovesCalc.java
@@ -0,0 +1,12 @@
+package chess;
+
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.HashSet;
+
+public class PieceMovesCalc {
+
+}
